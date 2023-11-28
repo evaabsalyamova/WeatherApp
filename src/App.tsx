087545989c83a09586e2a0c71fd9b8bf8572bbf7
evaabsalyamova <absalyamovaeva@gmail.com>
2023-11-28 20:23:12 +0400

    Create basic styles for `App`

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
