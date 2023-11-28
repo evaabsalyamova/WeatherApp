e7c462293c49063fc48199876b76d7c0ac70b546
evaabsalyamova <absalyamovaeva@gmail.com>
2023-11-28 21:18:43 +0400

    Create `LocalTime` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import SelectLocation from "./SelectLocation";
+import LocalTime from "./LocalTime";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="container">
         <div className="locationInfoBlock">
           <SelectLocation />
+          <LocalTime />
         </div>
       </div>
     </div>
---
+++ src/LocalTime/index.tsx
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+const LocalTime = () => {
+  return <div className="localTime">111</div>;
+};
+
+export default LocalTime;
