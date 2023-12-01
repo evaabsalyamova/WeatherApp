d7e42f80d45f301cfae0a37d12dd8c40e5ac1bfc
evaabsalyamova <absalyamovaeva@gmail.com>
2023-12-01 14:26:28 +0400

    Download city weather data after city changing

--- src/SelectLocation/index.tsx
+++ src/SelectLocation/index.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
+import { getWeather } from "../api/weather";
+import { addWeather, useAppDispatch } from "../redux";
 
 const SelectLocation = () => {
+  const [city, setCity] = useState<string>("");
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const response = getWeather(city);
+    response.then((data) =>
+      dispatch(
+        addWeather({
+          city: data.location.name,
+          temp: data.current.temp_c,
+          localTime: data.location.localtime,
+        })
+      )
+    );
+  }, [city, dispatch]);
+
   return (
-    <select className="selectOption" name="action">
+    <select
+      className="selectOption"
+      name="action"
+      onChange={(e) => setCity(e.target.value)}
+    >
       <option value=""></option>
       <option value="Yerevan">Yerevan</option>
-      <option value="Tbilisi">Tbilisi</option>
+      <option value="London">London</option>
     </select>
   );
 };
