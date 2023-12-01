408e557197e4c2b2e22209fbda34fa6c6597bca7
evaabsalyamova <absalyamovaeva@gmail.com>
2023-12-01 14:21:46 +0400

    Create basic redux structure

---
+++ src/redux/index.ts
@@ -0,0 +1,38 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
+import { IWeather } from "../types/weather";
+
+interface IAddWeatherAction {
+  payload: IWeather;
+  type: string;
+}
+
+const initialState: IWeather = {
+  city: "",
+  temp: 0,
+  localTime: "",
+};
+
+const weatherSlice = createSlice({
+  name: "Weather",
+  initialState,
+  reducers: {
+    addWeather: (_, action: IAddWeatherAction) => {
+      return action.payload;
+    },
+  },
+});
+
+export const { addWeather } = weatherSlice.actions;
+
+export const store = configureStore({
+  reducer: {
+    Weather: weatherSlice.reducer,
+  },
+});
+
+type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
