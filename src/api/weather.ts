7d331e30c87b3b82ded99d6523fb7e6c92c9b745
evaabsalyamova <absalyamovaeva@gmail.com>
2023-12-12 15:00:02 +0400

    Refactor api method to get forecast weather

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -6,7 +6,7 @@ const headers = {
 
 export const getForecast = async (city: string): Promise<IExternalWeather> => {
   const response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=b81ea357f1f24d9481290529232711&q=${city}&days=2`,
+    `http://api.weatherapi.com/v1/forecast.json?key=b81ea357f1f24d9481290529232711&q=${city}&days=3`,
     {
       method: "GET",
       headers,
